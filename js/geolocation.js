5a3b85e58aa76f2f90339c6f71f95c6175f23a68
jadman02 <jadman02@hotmail.com>
2014-09-08 08:33:20 +1000

    Update geolocation.js

--- js/geolocation.js
+++ js/geolocation.js
@@ -1,13 +1,24 @@
-navigator.geolocation.getCurrentPosition(
-        function onSuccess(position) {
-        alert('Latitude: '           + position.coords.latitude              + '<br />' +
+
+ document.addEventListener("deviceready", onDeviceReady, false);
+
+    // PhoneGap is ready
+    //
+    function onDeviceReady() {
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    }
+
+    // onSuccess Geolocation
+    //
+    function onSuccess(position) {
+        var element = document.getElementById('geolocation');
+        element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
                             'Longitude: '          + position.coords.longitude             + '<br />' +
                             'Altitude: '           + position.coords.altitude              + '<br />' +
                             'Accuracy: '           + position.coords.accuracy              + '<br />' +
                             'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
                             'Heading: '            + position.coords.heading               + '<br />' +
                             'Speed: '              + position.coords.speed                 + '<br />' +
-                            'Timestamp: '          + position.timestamp                    + '<br />');
+                            'Timestamp: '          + new Date(position.timestamp)          + '<br />';
     }
 
     // onError Callback receives a PositionError object
@@ -16,4 +27,3 @@ navigator.geolocation.getCurrentPosition(
         alert('code: '    + error.code    + '\n' +
               'message: ' + error.message + '\n');
     }
-};
