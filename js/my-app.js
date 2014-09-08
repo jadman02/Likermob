a25dbecac62423f0a1dcc350168eff0c85c0198f
jadman02 <jadman02@hotmail.com>
2014-09-08 12:49:41 +1000

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -52,7 +52,7 @@ var page_per_view = 10;
 	
 $$.getJSON('http://www.smilesavers.net.au/jsonp.php?callback=?', 'page=0', function(response){
 
-$$( '#result' ).append('yes');
+$$( '.page-content' ).append('yes');
 for (i = 0; i < response.length; i++) {        
 $$( '#result' ).append('<li><a href="#" class="item-link item-content"><div class="item-media"><img src="http://graph.facebook.com/'+response[i][2]+'/picture?width=120&height=120" /></div><div class="item-inner">'+ '<div class="item-title-row"><div class="item-title">Yellow Submarine</div><div class="item-after">$15</div></div><div class="item-subtitle">Beatles</div><div class="item-text">Lorem ipsum dolor sit amet...</div></div></a></li>');
 }
