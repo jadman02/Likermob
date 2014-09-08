24ed5f33ba1c5580f8fbb06ad7205d67ca8af720
jadman02 <jadman02@hotmail.com>
2014-09-08 13:16:29 +1000

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -72,7 +72,7 @@ $(".load_more").click(function (e) {
 
 
 if(track_click <= total_pages) {
-var offset == track_click * limit; 
+var offset = track_click * limit; 
 $$.getJSON('http://www.smilesavers.net.au/jsonp.php?callback=?', 'offset=' + offset, function(response){
 for (i = 0; i < response.length; i++) {        
 $$( '#result' ).append('<li><a href="#" class="item-link item-content"><div class="item-media"><img src="http://graph.facebook.com/'+response[i][2]+'/picture?width=120&height=120" /></div><div class="item-inner">'+ '<div class="item-title-row"><div class="item-title">Yellow Submarine</div><div class="item-after">$15</div></div><div class="item-subtitle">Beatles</div><div class="item-text">Lorem ipsum dolor sit amet...</div></div></a></li>');
