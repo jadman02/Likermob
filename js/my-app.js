56fc119ce983562022b339ec3dff2ee1c478a152
jadman02 <jadman02@hotmail.com>
2014-09-08 14:53:50 +1000

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -68,7 +68,7 @@ $$.getJSON('http://www.smilesavers.net.au/jsonp.php?callback=?', function(respon
 
 
 var i = 0;
-    while (i < 5) {
+    while (i > 5 && i<10) {
  $$( '#result' ).append('<li><a href="#" class="item-link item-content"><div class="item-media"><img src="http://graph.facebook.com/'+response[i][2]+'/picture?width=120&height=120" /></div><div class="item-inner">'+ '<div class="item-title-row"><div class="item-title">Yellow Submarine</div><div class="item-after">$15</div></div><div class="item-subtitle">Beatles</div><div class="item-text">Lorem ipsum dolor sit amet...</div></div></a></li>');
         i++;
     }
