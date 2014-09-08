373d0ed3bafb07ed3da8f66ecd232d9c9820b838
jadman02 <jadman02@hotmail.com>
2014-09-08 15:12:24 +1000

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -56,7 +56,7 @@ get_total_rows = response.length;
 total_pages = get_total_rows / 5;
 
 // Store
-localStorage.setItem("total_pages", response.length);
+localStorage.setItem("total_pages", total_pages);
 
 for (i = 0; i < 5; i++) {        
 $$( '#result' ).append('<li><a href="#" class="item-link item-content"><div class="item-media"><img src="http://graph.facebook.com/'+response[i][2]+'/picture?width=120&height=120" /></div><div class="item-inner">'+ '<div class="item-title-row"><div class="item-title">Yellow Submarine</div><div class="item-after">$15</div></div><div class="item-subtitle">Beatles</div><div class="item-text">Lorem ipsum dolor sit amet...</div></div></a></li>');
