fd3fef7c56e765209ca315438b67490e9ead40cf
jadman02 <jadman02@hotmail.com>
2014-09-08 13:39:40 +1000

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -56,14 +56,17 @@ var items = 5;
 
 for (i = 0; i < 5; i++) {        
 $$( '#result' ).append('<li><a href="#" class="item-link item-content"><div class="item-media"><img src="http://graph.facebook.com/'+response[i][2]+'/picture?width=120&height=120" /></div><div class="item-inner">'+ '<div class="item-title-row"><div class="item-title">Yellow Submarine</div><div class="item-after">$15</div></div><div class="item-subtitle">Beatles</div><div class="item-text">Lorem ipsum dolor sit amet...</div></div></a></li>');
-track_click++;
+
 }
-});
-
-
+track_click++;
 if (track_click==1){
 alert(track_click);
 }
+
+});
+
+
+
 
 
 
