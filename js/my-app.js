a64d51f21747dd09102436729445f58f63e5645f
jadman02 <jadman02@hotmail.com>
2014-09-08 12:34:34 +1000

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -14,18 +14,17 @@ var $$ = Dom7;
 myApp.onPageInit('index', function (page) {
   //Do something here with home page
 
-$$.getJSON('http://www.smilesavers.net.au/jsonp.php?callback=?', function(response){
 
+
+$$.getJSON('http://www.smilesavers.net.au/jsonp.php?callback=?', function(response){
 for (i = 0; i < response.length; i++) {        
 $$( '#result' ).append('<li><a href="#" class="item-link item-content"><div class="item-media"><img src="http://graph.facebook.com/'+response[i][2]+'/picture?width=120&height=120" /></div><div class="item-inner">'+ '<div class="item-title-row"><div class="item-title">Yellow Submarine</div><div class="item-after">$15</div></div><div class="item-subtitle">Beatles</div><div class="item-text">Lorem ipsum dolor sit amet...</div></div></a></li>');
 }	
-
 });
 
 
   
 
-});
 
 
 
