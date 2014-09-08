570255b319644ec70d75721a764c8a812d114c6a
jadman02 <jadman02@hotmail.com>
2014-09-08 10:16:47 +1000

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -18,7 +18,7 @@ $$.getJSON('http://www.smilesavers.net.au/jsonp.php?callback=?', function(respon
   
 for (i = 0; i < response.length; i++) {        
 
-$$( '#result' ).append( '<img src="http://graph.facebook.com/'+response[i][2]+'/picture?type=small"  />');
+$$( '#result' ).append( '<img style="height:30px;width:30px;" src="http://graph.facebook.com/'+response[i][2]+'/picture"  />');
 
 
 }
