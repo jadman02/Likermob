cb9a4c7162f21914576b873dfa7e721af1d12a93
jadman02 <jadman02@hotmail.com>
2014-09-09 14:01:22 +1000

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -60,9 +60,39 @@ $$( '#result' ).append('<li><a href="#" class="item-link item-content"><div clas
 
 }
 track_click++;
+$$(".load_previous").show();
 
 });
 
+
+$$(".load_previous").click(function (e) {
+
+$$(".load_more").removeAttr("disabled", "disabled");
+
+track_click--;
+	
+$$.getJSON('http://www.smilesavers.net.au/jsonp.php?callback=?', function(response){
+$$("#result li").remove();
+
+var start = track_click * 5;
+var finish = start + 5;
+
+for (i = start; i < finish; i++) {        
+$$( '#result' ).append('<li><a href="#" class="item-link item-content"><div class="item-media"><img src="http://graph.facebook.com/'+response[i][2]+'/picture?width=120&height=120" /></div><div class="item-inner">'+ '<div class="item-title-row"><div class="item-title">Yellow Submarine</div><div class="item-after">$15</div></div><div class="item-subtitle">Beatles</div><div class="item-text">Lorem ipsum dolor sit amet...</div></div></a></li>');
+
+}
+
+
+
+
+});
+
+
+
+
+	
+	
+});
 
 $$(".load_more").click(function (e) {
 
@@ -77,6 +107,7 @@ $$(".load_more").attr("disabled", "disabled");
 
 $$.getJSON('http://www.smilesavers.net.au/jsonp.php?callback=?', function(response){
 $$("#result li").remove();
+
 var start = track_click * 5;
 var finish = start + 5;
 
@@ -84,6 +115,8 @@ for (i = start; i < finish; i++) {
 $$( '#result' ).append('<li><a href="#" class="item-link item-content"><div class="item-media"><img src="http://graph.facebook.com/'+response[i][2]+'/picture?width=120&height=120" /></div><div class="item-inner">'+ '<div class="item-title-row"><div class="item-title">Yellow Submarine</div><div class="item-after">$15</div></div><div class="item-subtitle">Beatles</div><div class="item-text">Lorem ipsum dolor sit amet...</div></div></a></li>');
 
 }
+
+$$(".load_previous").show();
 
 track_click++;
 
