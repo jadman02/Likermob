e8ac01fda0609cf7d337ed4b13962b8906e61b80
jadman02 <jadman02@hotmail.com>
2014-09-11 09:02:59 +1000

    Update geolocation.js

--- js/geolocation.js
+++ js/geolocation.js
@@ -1,9 +1,30 @@
+ // Wait for Cordova to load
+    //
+    document.addEventListener("deviceready", onDeviceReady, false);
 
-
-
-    // PhoneGap is ready
+    // Cordova is ready
     //
     function onDeviceReady() {
-        alert('device ready');
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
     }
- document.addEventListener("deviceready", onDeviceReady, true);
+
+    // onSuccess Geolocation
+    //
+    function onSuccess(position) {
+        var element = document.getElementById('geolocation');
+        element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
+                            'Longitude: '          + position.coords.longitude             + '<br />' +
+                            'Altitude: '           + position.coords.altitude              + '<br />' +
+                            'Accuracy: '           + position.coords.accuracy              + '<br />' +
+                            'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
+                            'Heading: '            + position.coords.heading               + '<br />' +
+                            'Speed: '              + position.coords.speed                 + '<br />' +
+                            'Timestamp: '          +                                   position.timestamp          + '<br />';
+    }
+
+    // onError Callback receives a PositionError object
+    //
+    function onError(error) {
+        alert('code: '    + error.code    + '\n' +
+                'message: ' + error.message + '\n');
+    }
